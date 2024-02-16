3a8c839452522171bc5779de39514bdf916127f9
Perpi <jperpi01@gmail.com>
2024-02-16 18:11:54 +0100

    Corección salto de linea

--- src/funcions.py
+++ src/funcions.py
@@ -5,15 +5,15 @@ import math
 def es_primo(num):
     """
     Función para determinar si un número es primo:
-    
-    
+
+
     Entrada:
     - entero: número entero a determinar si es primo
-    
+
 
     Salida:
     - True/False: e caso de ser primo o no
-    
+
     """
     if num < 2:
         return False
