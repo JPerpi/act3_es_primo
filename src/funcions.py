8f68ba950774345de95ad20d3567012e50ba1239
Perpi <jperpi01@gmail.com>
2024-02-16 18:10:29 +0100

    Corrección de src puntuación 10

--- src/funcions.py
+++ src/funcions.py
@@ -1,7 +1,20 @@
+"""Módulo que contiene la función es_primo"""
 import math
 
 
 def es_primo(num):
+    """
+    Función para determinar si un número es primo:
+    
+    
+    Entrada:
+    - entero: número entero a determinar si es primo
+    
+
+    Salida:
+    - True/False: e caso de ser primo o no
+    
+    """
     if num < 2:
         return False
     for n in range(2, math.floor(math.sqrt(num) + 1)):
